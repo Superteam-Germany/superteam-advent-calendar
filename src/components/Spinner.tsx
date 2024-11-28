e1bbbad291abf49d6f4f4bb20c19d90d1e08657a
Janine Wagner <janine.vvagner@gmail.com>
2024-11-28 13:44:44 +0100

    improve styling

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,8 +1,8 @@
 export const Spinner = () => {
   return (
     <div className="flex items-center justify-center gap-2">
-      <div className="w-4 h-4 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
-      <span className="text-blue-500 text-sm">Minting Door...</span>
+      <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
+      <span className="text-white text-sm">Minting NFT...</span>
     </div>
   );
 }; 
