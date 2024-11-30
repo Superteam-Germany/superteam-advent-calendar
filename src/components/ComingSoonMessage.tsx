aa1624e281ac2d017342baf67ff5904dd73dfe3b
Janine Wagner <janine.vvagner@gmail.com>
2024-11-30 16:30:37 +0100

    change coming soon msg

--- src/components/ComingSoonMessage.tsx
+++ src/components/ComingSoonMessage.tsx
@@ -1,8 +1,9 @@
 export const ComingSoonMessage = () => {
   return (
     <div className="text-center p-8 max-w-2xl mx-auto">
-        <h2 className="text-2xl font-bold text-white mb-4">
-          Thank you for registering! The christmas raffles will begin on December 1st.
+        <h2 className="text-xl text-gray-300 mb-4">
+          Thank you for registering. Come back on 1 December to open your first advent calendar door.<br/><br/>
+          Open a door each day to unlock amazing gifts!
         </h2>
     </div>
   );
