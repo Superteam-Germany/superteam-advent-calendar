074929a0704d520f6edb9f4f98ebd9b72173ae7f
Janine Wagner <janine.vvagner@gmail.com>
2024-11-30 22:12:26 +0100

    fix eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Spinner } from '@/components/Spinner';
 import { motion } from 'framer-motion';
 import { useScroll, useTransform } from 'framer-motion';
 import { BlurredCard } from '@/components/blurred-card';
-import Doors from './sections/doors';
+// import Doors from './sections/doors';
 import { ComingSoonMessage } from '@/components/ComingSoonMessage';
 
 export default function Home() {
@@ -17,7 +17,7 @@ export default function Home() {
   const [isRegistered, setIsRegistered] = useState(false);
   const { scrollYProgress } = useScroll();
   const y = useTransform(scrollYProgress, [0, 1], ['-45%', '-0%']);
-  const testmode = process.env.NEXT_PUBLIC_TESTMODE;
+  // const testmode = process.env.NEXT_PUBLIC_TESTMODE;
 
   useEffect(() => {
     if (publicKey) {
@@ -98,11 +98,11 @@ export default function Home() {
     }
   };
 
-  const isBeforeDecFirst = () => {
-    const now = new Date();
-    const decFirst = new Date('2024-12-01T00:00:00Z'); // UTC time
-    return now < decFirst;
-  };
+  // const isBeforeDecFirst = () => {
+  //   const now = new Date();
+  //   const decFirst = new Date('2024-12-01T00:00:00Z'); // UTC time
+  //   return now < decFirst;
+  // };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-8 md:p-24">
